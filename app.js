ad01247bf4be1e74dc6c594008aa37e0f03e79ad
claudioKornfeld <mmckornfeld@gmail.com>
2021-04-04 10:38:47 -0300

    version final Kornfneld

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const path = require('path');
 const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'))
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+
 });
 app.listen(3030, () => {
     console.log('servidor corriendo en el puerto 3030');
